86f8afd0cca412b7f618c724e4384da17ab65644
Pimpi1988 <laurapimpignano@gmail.com>
2018-04-26 10:39:33 -0300

    My first commit

--- Testing_WebApplication/Controllers/HomeController.cs
+++ Testing_WebApplication/Controllers/HomeController.cs
@@ -1,37 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Testing_WebApplication.Models;
 
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
 namespace Testing_WebApplication.Controllers
 {
     public class HomeController : Controller
     {
+        // GET: /<controller>/
         public IActionResult Index()
+
+           
         {
-            return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            person person = new person()
+            {
+                firstname = "Laura",
+                lastname = "Pimpignano"
+               
+            };
+            return View(person);
         }
     }
 }
---
+++ Testing_WebApplication/Models/person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Testing_WebApplication.Models
+{
+    public class person
+    {
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+
+    }
+}
